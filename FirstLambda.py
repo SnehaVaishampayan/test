c788504f40a2be134b8c44eb137b332e9d20311b
sneha <sneha_gadre@yahoo.com>
2021-03-30 21:59:30 -0700

    Commiting from local eclipse

--- FirstLambda.py
+++ FirstLambda.py
@@ -1,7 +1,7 @@
 
 import json
 def lambda_handler(event, context):
-    message = 'Hello'
+    message = 'Hello from local eclipse'
     a = {
         'statusCode': 200,
         'body': json.dumps({'input': message})
