f49c71052dfd7f3de49295328a7151b5b68d754c
sneha <sneha_gadre@yahoo.com>
2021-03-31 22:15:24 -0700

    msg

--- FirstLambda.py
+++ FirstLambda.py
@@ -1,7 +1,7 @@
 
 import json
 def lambda_handler(event, context):
-    message = 'Hello from local eclipse 2'
+    message = 'Hello from local eclipse 23'
     a = {
         'statusCode': 200,
         'body': json.dumps({'input': message})
